299ebe0b6b7fe9672b1d3d049918b9f815a24e66
Luciano <lucco6823@gmail.com>
2021-11-02 17:26:33 +0100

    Introduzione di Bootstrap

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,21 @@
-<h1>HEADER</h1>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">COMICS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Inserisci un nuovo Comics</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Lista Comics</a>
+            </li>
+        </ul>
+        </div>
+    </nav>
+</div>
