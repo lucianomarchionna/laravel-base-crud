9048e1400b1337ea5daf042f695f4e98c744f175
Luciano <lucco6823@gmail.com>
2021-11-02 17:46:32 +0100

    Stampa in Html la lista dei Comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,7 +2,36 @@
 
 @section('content')
     <div class="container">
-        <h1>Indice Comic</h1>
-        @dump($comics)
+        <div class="row">
+            <div class="clo-12">
+                <h1>ALL COMICS</h1>
+            </div>
+        </div>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Series</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Sale date</th>
+                <th scope="col">Type</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <th scope="row">{{$comic['id']}}</th>
+                        <td>{{$comic['series']}}</td>
+                        <td>{{$comic['title']}}</td>
+                        <td>{!! $comic['description'] !!}</td>
+                        <td>{{$comic['price']}}</td>
+                        <td>{{$comic['sale_date']}}</td>
+                        <td>{{$comic['type']}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
